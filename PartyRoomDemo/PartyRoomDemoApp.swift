9ac80a910d60b5597bb139717eeb3b47cbadfa73
Chris Walter <cwalter@haverfordsd.net>
2026-04-29 08:01:52 -0400

    Initial Commit

---
+++ PartyRoomDemo/PartyRoomDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyRoomDemoApp.swift
+//  PartyRoomDemo
+//
+//  Created by Christopher Walter on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PartyRoomDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
